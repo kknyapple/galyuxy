112a20b1da9f8959cad20f26fba838689ab1728a
오기선 <pakox123@gmail.com>
2024-01-26 17:32:07 +0900

    init : 초기 생성
    Former-commit-id: f75c102ab9f14f88e0150fedc6bbd39d950d19d2

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:39f6891bebce856ce604ea450f08ace26fa1b931415985881fbb323f63ba26fb
-size 535
+oid sha256:edef9b911e1146a65c02eaa69cc9491d52f0db8510a6595d4a140e4ef0ad944c
+size 872
