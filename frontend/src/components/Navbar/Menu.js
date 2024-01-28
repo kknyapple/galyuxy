a14ae82f723e1df451eaeeb17d9c9d57e53cc3e7
giseon <pakox123@gmail.com>
2024-01-29 01:30:42 +0900

    feat : 네비바추가
    Former-commit-id: 5094ebace1ae8121d1e6169135583f0bc63bec02

---
+++ frontend/src/components/Navbar/Menu.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:14c538e2c4057cb84bd643bd4f5c6eb07bb4ec7b2c79462a3b095b81b9137116
+size 810
---
+++ frontend/src/components/Navbar/TeacherNav.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:793fa8457195d5a6829178f8d19bf4be839eb7004447e954cbdd4e0dfa99262a
+size 2169
