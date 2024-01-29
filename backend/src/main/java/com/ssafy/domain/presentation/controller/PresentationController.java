0cd3027340a64ae579d6360a1bde60c201c4b092
nayeon <kknyapple@gmail.com>
2024-01-29 16:00:31 +0900

    feat: 연극 및 발표 활동 삭제하기
    DELETE /presentation/{presentationId}
    
    
    Former-commit-id: 334c78c83d690d03619086a66b5f26e1e35dba64

--- backend/src/main/java/com/ssafy/domain/presentation/controller/PresentationController.java
+++ backend/src/main/java/com/ssafy/domain/presentation/controller/PresentationController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:af56111ff877761e7891ca4b8ca8b2ec8960a3f813a8795fb420fd1c8b818403
-size 3461
+oid sha256:c5a00c15651fbb2e8733577df4841aba7ccf1422f098e636409c7f0c36dfbaeb
+size 4264
