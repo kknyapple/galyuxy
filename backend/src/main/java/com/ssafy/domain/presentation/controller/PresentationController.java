4b84921791408dc4f0804f36b57dae6442096089
nayeon <kknyapple@gmail.com>
2024-01-29 15:07:11 +0900

    feat: 연극 및 발표 활동 추가하기
    POST /presentation
    
    
    Former-commit-id: cf2f899b1002f411e6973194435325ebd2165dd0

--- backend/src/main/java/com/ssafy/domain/presentation/controller/PresentationController.java
+++ backend/src/main/java/com/ssafy/domain/presentation/controller/PresentationController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:610516e0146ef98a14dd989159758685e9cf530d8b7186510cc44edbbe69cdba
-size 1111
+oid sha256:c4261617223717580b44a4a09cc9d6189718504bebaab2a90ebed09446effccd
+size 2191
--- backend/src/main/java/com/ssafy/domain/presentation/entity/Presentation.java
+++ backend/src/main/java/com/ssafy/domain/presentation/entity/Presentation.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:81d0231c8ecd20c839d2de65faaf7b2b79a17528c015859e3ad55e78e9ea7646
-size 890
+oid sha256:6dd6a7076196224eaad2200d1e2c8cbe4b437f6652a2931c99111806149253ea
+size 913
