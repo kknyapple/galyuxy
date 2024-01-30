c2a51cd0bcb4b6d5d418c78ae7af5281f0a6a05f
김현영 <hggew@naver.com>
2024-01-30 11:47:07 +0900

    feat: 문화유산 api 완료
    Former-commit-id: 913322134063a3aff4f8b0a33db6d8f4db69f5a9

--- backend/src/main/java/com/ssafy/domain/heritage/entity/Heritage.java
+++ backend/src/main/java/com/ssafy/domain/heritage/entity/Heritage.java
@@ -1,4 +1,3 @@
-package com.ssafy.domain.heritage.entity;
-
-public class Heritage {
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:f094b4180223cdb0f36d315f8c37b6b53fc0d6f94cce9ad1bb2d2bdf0ce5fc5b
+size 922
--- backend/src/main/java/com/ssafy/global/config/LoadDatabase.java
+++ backend/src/main/java/com/ssafy/global/config/LoadDatabase.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f4773528548a4de7ca69026ac83a332326bbb54eb47ecf1cde348235cbad501b
-size 1454
+oid sha256:8875dda9d8b4e8a3dacdb2bd17f83999a426af9648017aedae89bda575478c77
+size 2117
