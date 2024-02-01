932e797abc595b0b1c5f960155e3f6d47d758e74
nayeon <kknyapple@gmail.com>
2024-02-01 11:36:37 +0900

    refactor: 오타 수정
    Former-commit-id: a233908f0482f04ed6677adf2e3c6ef7f929afa0

--- backend/src/main/java/com/ssafy/domain/presentation/service/RoomService.java
+++ backend/src/main/java/com/ssafy/domain/presentation/service/RoomService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5eab521df2969b8e5c9b581f48cbaab6cabff5b6edc27676e74bb0cbec2a1f43
-size 1562
+oid sha256:86f4d4616b924bad02b033ad469057cc9af507e1e170d3e56a196ebf0e20d4c9
+size 1676
