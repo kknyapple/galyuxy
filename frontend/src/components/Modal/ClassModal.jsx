dbecbfc6b7f3e42e2e9fbc1799d3ad852eb68490
nayeon <kknyapple@gmail.com>
2024-02-03 00:15:57 +0900

    feat: 클래스 학생 추가
    Former-commit-id: c5ecff9f9caf7647224f0b290d033a1bf807ac9e

--- frontend/src/components/Modal/ClassModal.jsx
+++ frontend/src/components/Modal/ClassModal.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:853ee43c1a92caaee33668ac99d33bb4a6462b3ae9f385dc9063e896f65af0e4
-size 3326
+oid sha256:c6fb038c5ff3f8f8b2d58d9e20179a26dce9bbf0ec5ef6fa97c973175a6a8bd9
+size 4414
