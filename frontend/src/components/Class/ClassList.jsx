ae71f4a962fdf1d700084c603b33d37914e259bf
nayeon <kknyapple@gmail.com>
2024-02-02 14:29:07 +0900

    feat: 클래스 layout 및 dummy date 삽입
    Former-commit-id: 00ff2e05cc98b622bba59081a341b437313bfe10

---
+++ frontend/src/components/Class/ClassList.jsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:16cd6cfbffe90b6310636ed126dea0bc2720c645552c221401965c8e605bc81c
+size 4522
---
+++ frontend/src/pages/class/ClassPage.jsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:161a1ec22a8d8cc6cdcddffd5d687acbb36f6338523ea0df7a4c351ed34e2377
+size 2643
