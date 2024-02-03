ac5555eeb40ec4b29dbe893890aeaba7ffde9dfd
nayeon <kknyapple@gmail.com>
2024-02-03 20:01:19 +0900

    refactor: 학생 목록 분리
    Former-commit-id: 55b793506be8591e8296604e340d23a2cb918a95

--- frontend/src/components/Modal/ClassModal.jsx
+++ frontend/src/components/Modal/ClassModal.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b3ba61427ad28499f940a31acc6015311b540e3e3849c580721bfcab7ce64ff9
-size 5074
+oid sha256:b4ecf974c82bbf4b5a4c9dc72e3d6b4ea50a20272dcdc98b4554fabc8667d083
+size 5679
--- frontend/src/components/Modal/StudentList.jsx
+++ frontend/src/components/Modal/StudentList.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8db35669f920e29f4ba216e3524a994de5ea9faecdffcdd40338cfddf5bf420f
-size 2005
+oid sha256:1fc7c8018507f0602cf9df721ae68d5da04fa26f83080069492ad852c37044b4
+size 1649
