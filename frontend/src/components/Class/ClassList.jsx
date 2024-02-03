e71b167c327d98c16f66c9c8bbb74696401c73ed
nayeon <kknyapple@gmail.com>
2024-02-03 17:26:32 +0900

    design: 배경 삽입 및 클래스 css 수정
    Former-commit-id: 35b4ac767fc0f1befbcae260ff1ddbc8f521a44c

--- frontend/src/components/Class/ClassList.jsx
+++ frontend/src/components/Class/ClassList.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9f33d54626e29f0aa0c2c24cb5ada8893b548e0c80746f5fd79b7d8729c113e0
-size 5560
+oid sha256:6aa5e8b8d1b612bc587dc5367b01db280f9688ec0b28124de059139e8887d894
+size 5576
--- frontend/src/pages/class/ClassPage.jsx
+++ frontend/src/pages/class/ClassPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:141caeec262cc92843462f4fb083af0bddb4d4fc0ec634e0c127c1b00a4ec2ce
-size 3023
+oid sha256:0990b8909f642163d21e06082746093db8efe388dffec50dfea58b00a36b1f0c
+size 3268
