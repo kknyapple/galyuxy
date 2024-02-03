0eda85d5b4865df929c0c5d530e8eb38256cd3d2
nayeon <kknyapple@gmail.com>
2024-02-03 20:11:56 +0900

    feat: 학생 클래스 페이지 생성
    Former-commit-id: ac69cb262665e43f02bbe398e090dacc8733372a

--- frontend/src/pages/class/ClassPage.jsx
+++ frontend/src/pages/class/ClassPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0990b8909f642163d21e06082746093db8efe388dffec50dfea58b00a36b1f0c
-size 3268
+oid sha256:9ce1ce60c16cd34dd98515c034b3aa579232ca0495e64c5fb3b568f76ec3a315
+size 4966
