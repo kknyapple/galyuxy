c335e6ddb935399a565a4efe64d4679484a03582
nayeon <kknyapple@gmail.com>
2024-02-03 17:35:09 +0900

    feat: 클래스 삭제 시 confirm 창 추가
    Former-commit-id: 4aa5360dc70ee2a31291c2a91dea66e41b2ba3cd

--- frontend/src/components/Class/ClassList.jsx
+++ frontend/src/components/Class/ClassList.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e5342ddb9c98eb42c3a0454deee53414255b97c70216db4f7fb96d8ceb526114
-size 5898
+oid sha256:75717a8af153d6105e5778c1b1a8314833a3de9819f8e541719f8d77b8da8d6f
+size 6015
