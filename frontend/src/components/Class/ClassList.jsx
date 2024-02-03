05f3975cc4ac9dd7cbbc15607f9d28a2d65245f0
nayeon <kknyapple@gmail.com>
2024-02-03 17:32:32 +0900

    feat: 클래스 삭제 기능
    Former-commit-id: b256c1c7837cc3cf38335094c3fcc9496c710e6c

--- frontend/src/components/Class/ClassList.jsx
+++ frontend/src/components/Class/ClassList.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6aa5e8b8d1b612bc587dc5367b01db280f9688ec0b28124de059139e8887d894
-size 5576
+oid sha256:e5342ddb9c98eb42c3a0454deee53414255b97c70216db4f7fb96d8ceb526114
+size 5898
