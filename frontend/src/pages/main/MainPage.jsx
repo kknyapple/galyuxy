20db34dfd53d8d4742845009b04e5e97233d2406
조형찬 <bd3945@naver.com>
2024-02-04 17:04:05 +0900

    feat : CSS 메인화면 수정
    Former-commit-id: ba1193843f8d76c57fba56bec2bde0f6a8667c2e

--- frontend/src/pages/main/MainPage.jsx
+++ frontend/src/pages/main/MainPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4ab13edffd2d3eb21efef2d6f566a6a3b0f651b27199412d0dca08965535b8ea
-size 1366
+oid sha256:d69ef123d4d232ce8e154a7c2ef95b1ea6995096c7f7d84288cb6e7948fcef34
+size 1831
