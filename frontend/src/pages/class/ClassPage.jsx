fbb94c16fc3f1ee464fb16bb5b5b2ec3378bc60e
nayeon <kknyapple@gmail.com>
2024-02-04 16:19:41 +0900

    design: 클래스 페이지 css 변경
    Former-commit-id: 736bb41d7ff1956e2dafcfec4f8c8d9e3cda9c68

--- frontend/src/pages/class/ClassPage.jsx
+++ frontend/src/pages/class/ClassPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9ce1ce60c16cd34dd98515c034b3aa579232ca0495e64c5fb3b568f76ec3a315
-size 4966
+oid sha256:0e133fe22a6f1a4ae4294659e1aa1dc884f5277a22e5f627b8934464d16a0249
+size 4964
