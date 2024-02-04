f0d38dd1c2227e65fd634deaf8e259b128d96f89
nayeon <kknyapple@gmail.com>
2024-02-04 16:18:18 +0900

    feat: heritage 상태 설정
    Former-commit-id: e649c21dab7b7441c7fe5f1a56107832f6bf5be3

---
+++ frontend/src/Recoil/HeritageState.jsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f6c5b8f5be11e1b80e2e6a6c22cad506521bc233bd40f4153a3ea36943b44089
+size 813
--- frontend/src/pages/heritage/HeritageDetail.jsx
+++ frontend/src/pages/heritage/HeritageDetail.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1d67ccc4abde3bfe409137b1cfd08926e219a4fdb4aaf787da69b6417c617b9f
-size 6884
+oid sha256:73dfe4dd75c8eb0fb7a74ca1421374ea221f98f5ad02b6cbfc3433c6b6ddd95c
+size 6529
