4fed331c6b20a1a10cd332de947bc5279e44e647
nayeon <kknyapple@gmail.com>
2024-02-05 11:25:20 +0900

    remove: 불필요한 파일 삭제
    Former-commit-id: ce2d8675af2841b2d4ce370b4600845ec89fb702

--- frontend/src/components/Presentation/ActiveRoomList.jsx
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:4a65527d1b8dbf291ce3de3044727ca0d58cff0df85950c705c34a125b46ca81
-size 2040
--- frontend/src/pages/presentation/PresentationMain.jsx
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:2c34afa8f39290631558e896ecfae3dd6be65455bc35864fb86a0d868b943380
-size 564
