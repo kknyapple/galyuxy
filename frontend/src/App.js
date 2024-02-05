960d5249eaae8034a7a3024f1cb459daa55f4cce
nayeon <kknyapple@gmail.com>
2024-02-05 11:27:13 +0900

    refactor: 경로 삭제
    Former-commit-id: 366ff46d2f1f4b772dc054103b0d78f6efa44279

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7b4a53d70a90dc32cf2d077a9cc9513695d3d93a191f5b8f1c08e5764f9a44cf
-size 1805
+oid sha256:73a1c84f41287f0cb599ccf259542848051472023108d84095e2eb2af2daafa8
+size 1403
