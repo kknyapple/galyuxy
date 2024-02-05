663024ea0e155700cb28ba48f059628c058a3120
kkongch <dhwjdals1324@gmail.com>
2024-02-06 00:50:26 +0900

    feat : 컬러링북 페이지 일부 완성 및 css 수정
    Former-commit-id: a2c6520b0f9ccd4e8d83faf1840c42f00ed86cc7

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:73a1c84f41287f0cb599ccf259542848051472023108d84095e2eb2af2daafa8
-size 1403
+oid sha256:01ae3d1fb9c42f9a5fdda31ac8fac22ae911ab8add92f06cda28e1a7c8bf723a
+size 1460
---
+++ frontend/src/pages/art/Coloring.jsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d671fe9016b234b4da21fac0c19f03aa4fdf95a0beebab2bad30b207d0c5872b
+size 10740
