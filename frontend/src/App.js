dc81095dece9414227ec8cc77ebe57b52a85942d
nayeon <kknyapple@gmail.com>
2024-02-05 16:34:02 +0900

    feat: 페이지 경로 추가
    /room
    
    
    Former-commit-id: 6acea4d8dd926f3bc557d56dbd41baa2bbd01dc2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:73a1c84f41287f0cb599ccf259542848051472023108d84095e2eb2af2daafa8
-size 1403
+oid sha256:68237b58d406efffd79ecfaa3992e0b9356c699abf3522074535e6ddd80f7dcd
+size 1512
