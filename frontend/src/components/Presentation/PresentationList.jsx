ec3de539d21c32d34bc4a64939fe8103d13ef0eb
nayeon <kknyapple@gmail.com>
2024-02-05 11:18:27 +0900

    feat: 활동 목록 CRUD
    Former-commit-id: 91f908d2db6efecb9de1c3aa2a7b68b9856d8f95

---
+++ frontend/src/components/Presentation/PresentationList.jsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d6e0721c56c71f83eb4f031859c03f68bcd327577a5a868b5bfec4534600a7f8
+size 6085
---
+++ frontend/src/pages/presentation/PresentationPage.jsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8c8d7f2723687e7e1f7fa0d6e62009e424337245836e1c7a6df533b26b90c793
+size 3771
