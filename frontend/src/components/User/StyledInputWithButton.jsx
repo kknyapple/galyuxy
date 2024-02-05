e0cf7dd4c139d2766f2c54218cc0222ed27d1e95
nayeon <kknyapple@gmail.com>
2024-02-05 23:57:38 +0900

    feat: 회원가입 페이지 layout
    Former-commit-id: 2130a7e2a62962e1f8e2563dc01e2d11d7a29cb8

---
+++ frontend/src/components/User/StyledInputWithButton.jsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:859d1405f5318d5af42c691254c7c4c117485efff7d6a46195518fea88c1a867
+size 1075
---
+++ frontend/src/pages/user/SignUpPage.jsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2972f49d7988784ca864b9066332733cc3fbd165ed295f55c6592975fd0f5c6e
+size 2956
