1955c9c860c7053391b75dfeadec4b4cad9f6664
nayeon <kknyapple@gmail.com>
2024-02-05 20:10:23 +0900

    feat: 시작, 종료 버튼 토글
    Former-commit-id: 6fd0afe497bd93e2c73157816a369da509baa077

--- frontend/src/pages/presentation/RoomPage.jsx
+++ frontend/src/pages/presentation/RoomPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2172341cc9427c4e990991877ac4baab00a00e06f1cd54ea0f13d1aa8cd3d29a
-size 4682
+oid sha256:8d550f72c153a828ee3def2b8df2bd7e0b82b59309316c6893715fe1c9ed5769
+size 5003
