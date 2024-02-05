cf772a634f2eb8b1077d366c4e3677fe57363602
nayeon <kknyapple@gmail.com>
2024-02-05 20:05:13 +0900

    feat: 사용자 유형에 따른 화면 전환
    학생 사용자는 입장 버튼만 보이게 함
    
    
    Former-commit-id: 397c77d2cf85a04a67a6eec499545e8be8e074af

--- frontend/src/components/Presentation/RoomList.jsx
+++ frontend/src/components/Presentation/RoomList.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ed2dcc4afd6cd01876bb11ddab9eec4b489ba30ed799a2981c3d1ca0116368e5
-size 2675
+oid sha256:08e44493d54dbcc01cd60aa68db704a2d371c8e1e9fcf32bb470ead58a7c46f7
+size 2885
