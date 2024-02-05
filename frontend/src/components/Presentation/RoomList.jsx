0f02dba2750578aa674491bddc962a9ee2744856
nayeon <kknyapple@gmail.com>
2024-02-05 16:32:57 +0900

    feat: 방 CRD
    Former-commit-id: b42aa1a3da76ef79650c9c84ef8ae1539ee7e638

---
+++ frontend/src/components/Presentation/RoomList.jsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ed2dcc4afd6cd01876bb11ddab9eec4b489ba30ed799a2981c3d1ca0116368e5
+size 2675
---
+++ frontend/src/pages/presentation/RoomPage.jsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:20022fbfe69690bf81db57380c05b3e5dac979e2fb43c8998f6ed9b35f828330
+size 3844
