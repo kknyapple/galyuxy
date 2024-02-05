88ecce5c6790c5580b150af6c3b79315c3f1c237
nayeon <kknyapple@gmail.com>
2024-02-05 20:00:30 +0900

    feat: 방 주제 보여주기, 사용자 유형에 따른 화면 전환
    Former-commit-id: 40bfe778fcc4b726ac2644a0a0d32b40f47512ae

--- frontend/src/pages/presentation/RoomPage.jsx
+++ frontend/src/pages/presentation/RoomPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:20022fbfe69690bf81db57380c05b3e5dac979e2fb43c8998f6ed9b35f828330
-size 3844
+oid sha256:2172341cc9427c4e990991877ac4baab00a00e06f1cd54ea0f13d1aa8cd3d29a
+size 4682
