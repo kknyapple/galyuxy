1daf6fe2cf87b8da120e6b39f75cf2eefb194cb6
김현영 <hggew@naver.com>
2024-02-06 17:47:18 +0900

    feat: 그룹 수정, 삭제 메서드 추가
    Former-commit-id: fc56270d301dd3e6f9a6c78231af0b31b54574ff

--- backend/src/main/java/com/ssafy/domain/classroom/service/GroupService.java
+++ backend/src/main/java/com/ssafy/domain/classroom/service/GroupService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b5c2cdddd466f3d956e10133153bcb573d5c3c8165583d0ba7acdad0760f470f
-size 525
+oid sha256:8dd8bfbf165503a89a38b79000da00043009c1c57a43dfde510bbc9b35341a22
+size 596
--- backend/src/main/java/com/ssafy/domain/classroom/service/GroupServiceImpl.java
+++ backend/src/main/java/com/ssafy/domain/classroom/service/GroupServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4320ff7fc67cfdc7caebcad36961ded13936d63d44d8602513d74aa7c953e1cd
-size 2185
+oid sha256:12bee3640554f638f42e7f0192168a24fb0c8c0acfee37564182578bdf5dfbc0
+size 2858
