c282823e3c413708764bc415d172e314cbff598f
김현영 <hggew@naver.com>
2024-02-07 02:09:18 +0900

    feat: 클래스 수정 + 학생 수정api 완료
    Former-commit-id: 60199f89a5705f0c7e1d29096a99b2ae61ccb968

--- backend/src/main/java/com/ssafy/domain/classroom/controller/GroupController.java
+++ backend/src/main/java/com/ssafy/domain/classroom/controller/GroupController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:08cfb58d39b38f7e536458bde77ab3d81e9dfd082f76f4d0818641ac833e4755
-size 3356
+oid sha256:8ad06a226733db94c1008d948fa87826e287e46a6315e692dedbdbd6d3f2f094
+size 4319
--- backend/src/main/java/com/ssafy/domain/classroom/service/StudentServiceImpl.java
+++ backend/src/main/java/com/ssafy/domain/classroom/service/StudentServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3b02ac14c67dbefcce26205efee09de060bdd2d20c902fadb397de7f77191f85
-size 990
+oid sha256:71188876efc05f91aa62b761641dd572030b6fb234de680864e88edb6d5a650e
+size 1564
