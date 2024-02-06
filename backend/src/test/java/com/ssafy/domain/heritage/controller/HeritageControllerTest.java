afb9a79b3b6cb73b3b6fcc9fefd165c1dc945323
최지웅 <jwchoi42@naver.com>
2024-02-06 23:31:11 +0900

    fix: spring.jpa.hibernate.ddl-auto: update & CORS
    Former-commit-id: 7e5ea47cd8e0cb222c888d5725bf647f7862aec6

--- backend/src/test/java/com/ssafy/domain/heritage/controller/HeritageControllerTest.java
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:3babab43754838f578b26a8bed3128ed34f75294983e9b4a27551b7b647f89c6
-size 1965
--- backend/src/test/java/com/ssafy/domain/heritage/repository/HeritageRepositoryTest.java
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:fca52dcf2e39dd74feb0aab72a7daaf23423953c54c7d94459d28f31c543e28a
-size 843
