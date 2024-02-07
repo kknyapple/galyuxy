10493370ba4ad31b39d48c4741dafa863c342d01
nayeon <kknyapple@gmail.com>
2024-02-07 21:19:55 +0900

    feat: 로그인 토큰 session 저장
    Former-commit-id: e60d5f94d3d7df973fc75648208f498894fe7578

--- frontend/src/pages/user/LoginPage.jsx
+++ frontend/src/pages/user/LoginPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d8fe7ff4633040e76437ddcce0b3d70c21538edd1b6c201c4620932e8de173d8
-size 3757
+oid sha256:c79aeaa84996ccd2ed992d015f366558660ac849f26cc38dea03bd48fb8b6670
+size 4047
