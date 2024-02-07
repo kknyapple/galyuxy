091a1694090a36a759090b139daa31239a86e97b
nayeon <kknyapple@gmail.com>
2024-02-07 12:11:47 +0900

    feat: openvidu connection 생성 함수 제작
    Former-commit-id: 2cf8e8d96bd85fbcadd53e896329143b4f3fec64

--- frontend/src/api/RoomApi.js
+++ frontend/src/api/RoomApi.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:16124dae389a34d9428001fd5b1f0fe007ba259d9b912404771e3aac49bda367
-size 1112
+oid sha256:ba5c14d17af5d24c1c7363f343bd279d71458c8bd9517d2d7c8170de28468f8d
+size 1404
