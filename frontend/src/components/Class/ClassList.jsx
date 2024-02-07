aff0a05673c252c0e50f0e349b59c9d37e0af30b
nayeon <kknyapple@gmail.com>
2024-02-07 22:03:00 +0900

    feat: 교사 사용자가 이전에 생성했던 모든 클래스 목록 출력
    GET /group/list
    
    
    Former-commit-id: e2e64612390c45d70d7638aa6016b750b97c6cb5

--- frontend/src/components/Class/ClassList.jsx
+++ frontend/src/components/Class/ClassList.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e9292412c280a8c82b47dede30b8abf1f5f1dbe5072a0af5dc1d35ace581f984
-size 6073
+oid sha256:4051558116acf81414d7e25c3badd365b8dcc68d664ccee7836b5a7abc7d3ef2
+size 5630
