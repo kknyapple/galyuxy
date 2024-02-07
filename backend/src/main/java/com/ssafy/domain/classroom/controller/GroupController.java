642a1e4fb066cf40a62e1f9a8061a088f47dc765
김현영 <hggew@naver.com>
2024-02-07 10:23:25 +0900

    refactor: 클래스 수정 url 변경
    Former-commit-id: 86afaa6101c6a4a4ebd56b5f5887ea048f0eed10

--- backend/src/main/java/com/ssafy/domain/classroom/controller/GroupController.java
+++ backend/src/main/java/com/ssafy/domain/classroom/controller/GroupController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f85dae2edf9c4e93e291103758456ba8d1d62f288862c3fc2ac3901ec4e0ffb0
-size 4947
+oid sha256:edd81d9e8006ce8a64faa65a7a569a4a6fca9d2ce7dc92cd45d9ceef6d2f6152
+size 4944
