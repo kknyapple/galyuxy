e5b1e9011e9c2f09b4684037195c90b677bedab3
오정민 <dhwjdals1324@gmail.com>
2024-02-08 14:58:47 +0900

    feat: 퀴즈 페이지 수정중
    Former-commit-id: fee26dded23ea27156c809d5c5002f3100fce264

--- frontend/src/pages/quiz/QuizEnter.jsx
+++ frontend/src/pages/quiz/QuizEnter.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:119656bb00683a0c71e883b2af6c532995f5049e1bd632edaa23d5f039537857
-size 1697
+oid sha256:bdd6fa2ccfa947951663a3bb0c83d8b875012dd81dd97f493caf5c94030e7dc7
+size 1827
---
+++ frontend/src/pages/quiz/QuizStudentWait.jsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a0afab3e3a5f16114579d92e4741ab17e1dec9810e6da9d9e7e7e5c80dfd5661
+size 550
