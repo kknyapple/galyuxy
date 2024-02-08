7c384eccd6e90838feda00659c80d6c76d043a9e
오정민 <dhwjdals1324@gmail.com>
2024-02-08 10:24:36 +0900

    feat : heritage api붙이기 성공 및 state로 상태관리
    Former-commit-id: c304eaf85a84c1fe79f84417a49bb0c9726f34f0

--- frontend/src/pages/heritage/HeritageList.jsx
+++ frontend/src/pages/heritage/HeritageList.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e8af9cdfcfd66e0b62fbb20590b93157bdb024a8020818342f917d5657128de7
-size 3356
+oid sha256:0dce9e30a49d634f7db6d1c338b93e5fa803deba2fbbc32528ed19e6fb070bf2
+size 3675
