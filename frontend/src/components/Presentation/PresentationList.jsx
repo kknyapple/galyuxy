a6d53eea18fc9c46372a73f85192c28f02e0ce6b
nayeon <kknyapple@gmail.com>
2024-02-08 12:14:36 +0900

    fix: 발표 및 연극 활동 추가 후 목록 출력 수정
    활동 목록에 id 값 추가
    
    
    Former-commit-id: af3489a4db0360b7dba74685059fbeebcaf39be0

--- frontend/src/components/Presentation/PresentationList.jsx
+++ frontend/src/components/Presentation/PresentationList.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c7e75cea96f5e29ae81e1b65421d05ba707040f387f573e8ef92952dc26873ef
-size 6904
+oid sha256:0d7c7c6712d7430c4e002d1a555c8d6f020346d8f1571061643cef4acc787814
+size 7186
--- frontend/src/components/Presentation/PresentationModal.jsx
+++ frontend/src/components/Presentation/PresentationModal.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4b6e2ee8f6ef53d912d623f8c4ceac2cf665d8f10aaa8b9f3ad0aa17ff28b183
-size 4873
+oid sha256:28cdb1d0537f1c9a065da30a2a65cfc4107f2905b0eda8a5f827f57fc1237804
+size 4782
