943a0be302c14c72e6e937299eae9f6e173d5c5b
최지웅 <jwchoi42@naver.com>
2024-02-09 18:47:08 +0900

    feat: 실시간 퀴즈 참여자 기능
    Former-commit-id: d407276e10a528fcf9322bc4ebfeb8bc93bb3b5c

---
+++ backend/src/main/java/com/ssafy/domain/quiz/controller/QuizParticipantController.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2f04c1c8b3e462a667b566db927676bd02ce98f31c0a9386ea7ec110d986013b
+size 612
---
+++ backend/src/main/java/com/ssafy/global/config/WebSocketConfig.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:56a4e702ef4951fd739ec4e8e44bf63836495e27971ba971025d33b1c5381051
+size 898
