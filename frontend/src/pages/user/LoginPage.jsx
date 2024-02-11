495d1b5b7ba979c585bd2c6e874f12f586652085
nayeon <kknyapple@gmail.com>
2024-02-11 16:25:32 +0900

    feat: 로그인 후 클래스 선택 페이지 이동 및 로그인 실패 시 alert 창 띄우기
    Former-commit-id: a28818b5ac2cedad4199990bea44f1c565299513

--- frontend/src/pages/user/LoginPage.jsx
+++ frontend/src/pages/user/LoginPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:84ac2d67afde773b4b38d3216bc3caaec3a6b73e09a4876070bcde5ac7612fa1
-size 4344
+oid sha256:6224a79cb855501b76b72484ee29a6aee420a6f46cd8278594000abc93660f58
+size 4365
