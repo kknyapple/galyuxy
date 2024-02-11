4a288ea05c70c37a1c57e5d98dc998f1a03ec51e
조형찬 <bd3945@naver.com>
2024-02-12 01:25:05 +0900

    feat: 아트페이지 수정
    Former-commit-id: 3790f64f35e52225be8829e6df2658f50eb3abb7

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5f6cdac17f2cacfbb4cfa87e0503f8cc9b2d9a7f313b797cab690e6d017eb3d4
-size 2609
+oid sha256:ced5e68cb7d3d51128fba57702e9d2587d18266829eed37bf63af4c677bf9fec
+size 2649
