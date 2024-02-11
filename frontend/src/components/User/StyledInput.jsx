20404152678e46cf783bf631ebceacc59a5bb865
nayeon <kknyapple@gmail.com>
2024-02-11 16:12:21 +0900

    feat: 이메일 인증 성공 후 input 입력 막기
    Former-commit-id: 1afd0dd525ed9b78d60d0d07b800cbfb1c0d54c5

--- frontend/src/components/User/StyledInput.jsx
+++ frontend/src/components/User/StyledInput.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b4c8b3b70bc8fe5746d13b551c6055e818fdb0823d8b21e54ace0dd6693130b8
-size 638
+oid sha256:8a3dd2c38b7ac21a0324e2ac0d6a0b0715bed96593dd715e3f1bcad08cd60beb
+size 728
--- frontend/src/components/User/StyledInputWithButton.jsx
+++ frontend/src/components/User/StyledInputWithButton.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2d6d531f1bc6f01ba7c67c06e3d72681b5a24fdc60eacf56435c503c04ebd0e8
-size 1191
+oid sha256:bedf555ebaa0848b6e317e77525810bd2697a41575ed29a19d5b22ab62a7a65f
+size 1231
