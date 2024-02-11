3569ca4bb3b1daee0972ecb47f0a2983ad01b970
nayeon <kknyapple@gmail.com>
2024-02-11 15:56:27 +0900

    design: 버튼 커서 추가
    Former-commit-id: 94a62c0be0a1f22c429e21e17a482015038e5340

--- frontend/src/pages/user/LoginPage.jsx
+++ frontend/src/pages/user/LoginPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c79aeaa84996ccd2ed992d015f366558660ac849f26cc38dea03bd48fb8b6670
-size 4047
+oid sha256:84ac2d67afde773b4b38d3216bc3caaec3a6b73e09a4876070bcde5ac7612fa1
+size 4344
