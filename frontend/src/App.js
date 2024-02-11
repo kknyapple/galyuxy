23b3c618fadfa4635fa8f8d7167590bf0ac3a473
kkongch <dhwjdals1324@gmail.com>
2024-02-12 01:48:27 +0900

    feat : 퀴즈 문제풀이(학생) ox파트 api연결 제외
    Former-commit-id: 7684b0e14c1f3ef68a933f5ff740616aed039624

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5b9855777e389d2ffaf1ed844ae2ed1c9727f78f8cd5f6f7cd0cf403a43c21d7
-size 2540
+oid sha256:26632048670fed6b40091cbf4b589aa5805580c40ca5a1bffade8b5df979d9c2
+size 2649
