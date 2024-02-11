76c10c51db48e7ef749862a5a2c1f939953d757f
최지웅 <jwchoi42@naver.com>
2024-02-11 16:29:49 +0900

    feat: 연극 및 발표활동 활성화/비활성화, 활성화된 발표 활동 가져오기
    Former-commit-id: 31d2809c4ec4392ec2e3bbfabc9f3c237601fb93

--- backend/src/main/java/com/ssafy/domain/presentation/service/PresentationService.java
+++ backend/src/main/java/com/ssafy/domain/presentation/service/PresentationService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a5e9d81c31f213b29df813c90068fc9fee4307b470c3c2e602c3d13b319b4f0b
-size 2404
+oid sha256:73800177160f2dfff1f3e490b508e891fa1e2eb17e33818b8d5e6f3ee2bbcf5f
+size 3209
