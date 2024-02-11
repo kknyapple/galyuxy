135165ae6de93da34e6487624192fe79469d3b5d
nayeon <kknyapple@gmail.com>
2024-02-11 16:22:18 +0900

    feat: 회원가입 input 유효성 검사
    input 빈 값 검사, 비밀번호 확인 검사
    
    
    Former-commit-id: 6bb1ef256c70be8cc95af10111104b65eeec0a21

--- frontend/src/pages/user/SignUpPage.jsx
+++ frontend/src/pages/user/SignUpPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6438fc8f8c7098c755cd60c0984ffb006ee4969ea39db4796689eebd8682cb69
-size 5114
+oid sha256:b5a9169cf056b7dc6b7d41d0e365b0239b448c6b76407fcb8823e666dbb04bf0
+size 5495
