0e358da65e8804271449bf2f6c84a838a845b2b1
김현영 <hggew@naver.com>
2024-02-13 00:19:55 +0900

    feat: 카메라 크기 변경
    Former-commit-id: b6250d61200ed2a293a37e3cce0ead5341bf07c5

--- frontend/src/pages/art/ArtCamera.jsx
+++ frontend/src/pages/art/ArtCamera.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:54ee645c51f5b23618c8d71724d6cfeaf2a5686109ea8f625e099574b6963b6c
-size 5913
+oid sha256:4bd277ffbb28e95ddff5a1f58303735a043287fb3b705afff2dca3d775c6b309
+size 5915
