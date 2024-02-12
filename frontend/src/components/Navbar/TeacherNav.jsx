074ea6bab39f6f3cc0fc20917d14cdd96cffa3e8
nayeon <kknyapple@gmail.com>
2024-02-13 00:02:05 +0900

    feat: 클래스 선택 버튼 클릭 시 네비게이션바 닫기
    Former-commit-id: 8d7ade66ba0b6d2c458d313102d30a1fab682702

--- frontend/src/components/Navbar/TeacherNav.jsx
+++ frontend/src/components/Navbar/TeacherNav.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:df973dcc0653bbfd0f803fde1becae2565ad263a5d6fe65de42978381de6fc04
-size 7801
+oid sha256:9431c4176ac864d5409028d2961fa9a2387860ca359d1bf44acb99368a70a909
+size 7823
