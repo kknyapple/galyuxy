fa675835500316340bdc40c15e251cbd01d2bb4b
nayeon <kknyapple@gmail.com>
2024-02-12 23:27:55 +0900

    feat: 메인 화면에서 네비게이션 바 보이기
    Former-commit-id: 52a6a9fba040ec7c40acffe22728c8d3002e0802

--- frontend/src/Recoil/UserState.jsx
+++ frontend/src/Recoil/UserState.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cb96c191c1823abc80821e589073104378297058bbe18a16f89e87dd2b72049b
-size 663
+oid sha256:d3ce318b13ffbebd63038ea4defcf28260eb72e370778c74a5d86a2961605cae
+size 662
