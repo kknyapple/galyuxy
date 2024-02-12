b2cf7a2e5442d7a7877ac47fde5dc9986c80ab36
nayeon <kknyapple@gmail.com>
2024-02-12 23:04:14 +0900

    feat: 로그인 전 네비게이션 출력, profile 클릭 시 로그인 페이지로 이동
    Former-commit-id: 761cc6da12b65456ccc15ce06a613d359d60d5e2

--- frontend/src/components/Navbar/StudentNav.jsx
+++ frontend/src/components/Navbar/StudentNav.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2524e4dab20ca5ca5c3dd510753d868d5f660da65d14beb3453c9d5bddbfeefb
-size 6357
+oid sha256:04ceb15ab70df08c1d4ce0063b6e7aa9076291e37deb4f29ba67a4182749f598
+size 7145
