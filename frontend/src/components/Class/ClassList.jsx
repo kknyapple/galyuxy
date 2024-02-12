757fd89e2ed5735a7e482b29bc4dec747c7bb51a
nayeon <kknyapple@gmail.com>
2024-02-12 16:22:54 +0900

    feat: 입장한 클래스 그룹 id 및 교사 사용자 이름 session storage 저장
    Former-commit-id: 278e1916496ac3806701cc77b3f3776a18c382a3

--- frontend/src/components/Class/ClassList.jsx
+++ frontend/src/components/Class/ClassList.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a877767ae500c0adeaafdd8024350a46235565f874a3bc0f6b85304714390b05
-size 6853
+oid sha256:01f6d0a24b44027271e10d1de61a7cc77ad0079d5ce25ee7d538274625a4e57e
+size 6855
--- frontend/src/components/Navbar/TeacherNav.jsx
+++ frontend/src/components/Navbar/TeacherNav.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:657c09a99575abc20c6835ea83bd4507e9887188a2a32327901a4cacfa263d6d
-size 7240
+oid sha256:42d842311007d03bb6adf04d05ffb3e128f3c0e114a84ccff5e92e17b44b36be
+size 7256
