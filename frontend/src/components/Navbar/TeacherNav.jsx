6934f37974fa79d32f2999580919131f172c132b
nayeon <kknyapple@gmail.com>
2024-02-12 16:44:47 +0900

    fix: name key session storage 삭제
    Former-commit-id: 0ca92a3fdd43d2fc3a933c6867074b3953625541

--- frontend/src/components/Navbar/TeacherNav.jsx
+++ frontend/src/components/Navbar/TeacherNav.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:42d842311007d03bb6adf04d05ffb3e128f3c0e114a84ccff5e92e17b44b36be
-size 7256
+oid sha256:f3c0a1e05b4724e35aaf13a47807bbfb9766405ad91e38ebcda72e17ab7649eb
+size 7295
