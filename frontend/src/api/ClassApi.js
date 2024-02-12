45da9b3affa0208be5450868234c9e639d07a011
nayeon <kknyapple@gmail.com>
2024-02-12 15:38:39 +0900

    feat: 클래스 삭제
    PUT /group/delete/{groupId}
    
    
    Former-commit-id: 360427b001def99538708c3cb46d7d480b81a79f

--- frontend/src/api/ClassApi.js
+++ frontend/src/api/ClassApi.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aa8a6b6192dbd6bef319b7237d0b197bc19d779193fbbc6e6700eb0d5cf4fb9e
-size 1112
+oid sha256:976457509ed554c1af063b9703fc050b67b48b78f3d6bd3cd9575cb21eb12a49
+size 1412
--- frontend/src/components/Class/ClassList.jsx
+++ frontend/src/components/Class/ClassList.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7a3b22ac306cade03eda9fda004ed363d6d3cb9d7ec4003cbab6083f04c93cd6
-size 6088
+oid sha256:a877767ae500c0adeaafdd8024350a46235565f874a3bc0f6b85304714390b05
+size 6853
