9b19412fb334c10ce9f9c6b2f89dd0aeedfc2be6
kkongch <dhwjdals1324@gmail.com>
2024-02-12 15:56:03 +0900

    feat : 퀴즈 끝난 후 화면 api연결 제외 완성
    Former-commit-id: fc9244af4a15ad7adea6761c729bda4f358b949e

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:26632048670fed6b40091cbf4b589aa5805580c40ca5a1bffade8b5df979d9c2
-size 2649
+oid sha256:d245347886ddc602ec41bf4bc532420a3b34595411130165e915c2f92324df97
+size 2761
