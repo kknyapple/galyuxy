77a18af392f69fd2577679a64f6a41dddab95012
김현영 <hggew@naver.com>
2024-02-14 00:30:18 +0900

    feat: 그림판 위 사진 크기 조정
    Former-commit-id: e09102600af4dad446b7f4aa3fea55e1469b2a09

--- frontend/src/pages/art/Coloring.jsx
+++ frontend/src/pages/art/Coloring.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aa3b8e854c18f58fd6735cbc67f4b4047b6045bb6cc0d08eb6ecc9a46f97f5aa
-size 11829
+oid sha256:84bf744c75a1cdb55737064c7068b064dad9838ca0eac4c47c5ea33c0fc6d0e6
+size 13784
