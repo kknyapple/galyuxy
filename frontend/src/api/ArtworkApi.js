3a4afca038bbd8a01266debfdd622d6f475bb771
김현영 <hggew@naver.com>
2024-02-13 22:18:03 +0900

    feat: 미술활동 메뉴 페이지, 컬러링/드로잉 페이지로 넘어가기 완료
    Former-commit-id: 42f65cc6d000cf7e21fda80135880c8f131085f8

--- frontend/src/api/ArtworkApi.js
+++ frontend/src/api/ArtworkApi.js
@@ -1,22 +1,3 @@
-import axios from 'axios';
-
-const baseURL = process.env.REACT_APP_BASE_URL;
-
-const api = axios.create({
-  baseURL,
-});
-
-
-
-export const getArtworkList = async () => {
-    try {
-      const response = await api.get('/artwork', { 
-      });
-      console.log(response.data.dataBody);
-      return response.data.dataBody;
-    } catch (error) {
-      console.error('Error getClassList:', error);
-      throw error;
-    }
-  };
- 
+version https://git-lfs.github.com/spec/v1
+oid sha256:e02f12183bab094bf1a81af4dd2c6d1fdd8197d08d442782e62d4a173d75ee0e
+size 546
--- frontend/src/pages/art/ArtPage.jsx
+++ frontend/src/pages/art/ArtPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6a7dc8306c51355d5ace73d18f2bc0228f4cefc32a59f40ca991ed1e44de9307
-size 5425
+oid sha256:3f656d32614f815fe362ccca91656d321d1918e70f12d0bed6abeee982026358
+size 6463
