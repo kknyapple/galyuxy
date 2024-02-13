149801b7165304b24bda2cbdd20d5ddad9617817
nayeon <kknyapple@gmail.com>
2024-02-13 16:26:25 +0900

    refactor: 불필요한 코드 삭제
    Former-commit-id: d695b0f58d570ab234f2de9cede47ba3fcc659f2

--- frontend/src/pages/heritage/HeritageList.jsx
+++ frontend/src/pages/heritage/HeritageList.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:feab70c348efe23c5aa5b0fcbc5ef41203b88213d6dd96004d99b2a83e3187d8
-size 3678
+oid sha256:7fe655d9e0788c8961094279bc25a2bc508ae15c616830d225a8a518ece5d24d
+size 3052
