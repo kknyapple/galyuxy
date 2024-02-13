e56f15abd5b26238775ef181562e9bb6e843f2b5
nayeon <kknyapple@gmail.com>
2024-02-13 11:15:03 +0900

    fix: base url 수정, sessionId 오류 수정
    Former-commit-id: d30ebf12f09a4c47c968e02974c0daab4927c140

--- frontend/src/components/Presentation/openvidu/components/VideoRoomComponent.js
+++ frontend/src/components/Presentation/openvidu/components/VideoRoomComponent.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a9bcad805f0868c90abee852c4398b3b7c1da2e01f7f07217dc07b8803c948f2
-size 20363
+oid sha256:58e29cc194d8be655e91d8efce9317cbaef25cd8112c0854dcf6e07e28f2754c
+size 20379
