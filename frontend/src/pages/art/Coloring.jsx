af514028eabd7ef7ef3d96ff41c3b7ca841e75d5
김현영 <hggew@naver.com>
2024-02-14 01:47:39 +0900

    feat: 그림판 이미지와 그림 동시 저장
    Former-commit-id: bb494b1cdc8b0a2c8c23d77f8b938ae0deaec875

--- frontend/src/pages/art/Coloring.jsx
+++ frontend/src/pages/art/Coloring.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:84bf744c75a1cdb55737064c7068b064dad9838ca0eac4c47c5ea33c0fc6d0e6
-size 13784
+oid sha256:7587a651702b1a41856656fbee5bf00d886f97657c335263285b990124fad943
+size 15075
