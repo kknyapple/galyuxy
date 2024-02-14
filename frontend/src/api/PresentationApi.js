ab1f5e59fd254f59c10e8de0350643b592e31b2f
오기선 <pakox123@gmail.com>
2024-02-14 22:35:52 +0900

    feat : 룸페이지 활성화 api추가
    Former-commit-id: 3d6958b074778b6471e651182a6ca74cff6f8314

--- frontend/src/api/PresentationApi.js
+++ frontend/src/api/PresentationApi.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:07a402441d277fd29bb321797743790f9917c959daa227590276b68f4824cd86
-size 1317
+oid sha256:0baa3881642badc64b168ea638bf9a5b325af1cac5fdfebc0bf1e7a617721a51
+size 1946
