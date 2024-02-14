2251116d9d3eaa35f7dbbb9ead661034ecfa2ad9
오기선 <pakox123@gmail.com>
2024-02-14 22:19:44 +0900

    feat : 비디오페이지 주석제거
    Former-commit-id: 165d7dc0645c9798c11a3817999a43c6a3498c56

--- frontend/src/components/Presentation/openvidu/components/chat/ChatComponent.js
+++ frontend/src/components/Presentation/openvidu/components/chat/ChatComponent.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a2453ebce866b6a36f6d19c6ee59897acb3ff4d6673111faad6396ee8e0f8c2a
-size 5387
+oid sha256:b7f767fc1dc883549de30a537cf97270e6ad236225589464251735ae75370940
+size 4662
