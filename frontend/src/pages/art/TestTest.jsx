df40379d2e9b0637d814413810ead367fd01e5bf
김현영 <hggew@naver.com>
2024-02-15 02:51:11 +0900

    feat: testpage camera연결- 화면x
    Former-commit-id: 8caddd62dedea3513a31b2b7d68eb3ccda8ebca9

--- frontend/src/pages/art/TestTest.jsx
+++ frontend/src/pages/art/TestTest.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:07726ad46ecdb2ce85231128f3bc208e121cafb7facfbba2f355db463d1b161a
-size 5116
+oid sha256:0cbfe226843c0b8f1938181021bdd76020522432e28cc131b68e85e768d6b436
+size 9562
