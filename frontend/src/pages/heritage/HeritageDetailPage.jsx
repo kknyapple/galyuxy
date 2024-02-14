29a81491ed8e78b6e0594a2273b1af5e63fcf6a4
nayeon <kknyapple@gmail.com>
2024-02-14 21:52:05 +0900

    fix: 라우터 수정
    Former-commit-id: 7f7a649da270cdb359466b25534b46dfa6fcb1b8

--- frontend/src/pages/heritage/HeritageDetailPage.jsx
+++ frontend/src/pages/heritage/HeritageDetailPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:44cf762176ed97dc75014ff7efea1c7e889b2ef4540bf28d8b57650c1761dba7
-size 6911
+oid sha256:cb0a7f2b25ec077df7c073ee9e0664202e2f4e17eb181653f613c5039115f2e8
+size 6920
