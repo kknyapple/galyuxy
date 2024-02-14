d5d4383b74eb6b81b9a87b23e07d772678024b75
nayeon <kknyapple@gmail.com>
2024-02-14 16:01:02 +0900

    feat: 3D 문화유산 출력
    Former-commit-id: 3d707ba39519756030cff148564ee62d1291cc9d

--- frontend/src/pages/heritage/Heritage3D.jsx
+++ frontend/src/pages/heritage/Heritage3D.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2b0ddbcd4cc08b7fb02d76184c0c0a242e427a9369f9ea313b1aa1080ef481ae
-size 5665
+oid sha256:7a0ec1561ed0dd0224255198b6ae0d9bf52f6dbbc0f5be17f2ef31a919826bf4
+size 6961
