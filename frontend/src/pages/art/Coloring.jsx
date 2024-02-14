6aeb1b6bbb6ec75519c2aa12aa819dbd7b5f6aaa
nayeon <kknyapple@gmail.com>
2024-02-15 01:29:57 +0900

    design: 컬러링북 이미지 크기 조정
    Former-commit-id: de37238baf225da1fd267930b433b64508ce24b5

--- frontend/src/pages/art/Coloring.jsx
+++ frontend/src/pages/art/Coloring.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4ef29de5b05726873886bff22dfb4a8e068d099bdc5a67407db33cc378ad4af3
+oid sha256:a092efb4c77ff9d8767c0f530742ce65808a71d1aab8e300c50056c21d54bed4
 size 19824
