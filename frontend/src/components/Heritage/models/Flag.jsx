9613b43628d15e30f0cd976e6bc4809b2b2bbb5d
nayeon <kknyapple@gmail.com>
2024-02-14 20:35:06 +0900

    feat: 3d 문화유산 컴포넌트 분리
    Former-commit-id: c0b9707233627b5566dbecbde613840c34b5402c

---
+++ frontend/src/components/Heritage/models/Flag.jsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:05d251fe2380a08af85666083396b6d76ac7ee31fbe788a67635005b4179bffc
+size 2398
---
+++ frontend/src/components/Heritage/models/HemisphericalSundial.jsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e0cd0de08761a0a4d6856cc7e7f070c153de08866310e19d02b71a2f7b636b87
+size 620
