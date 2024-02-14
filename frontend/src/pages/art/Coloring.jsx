27c9d69d1f2f179978804c194c4cc87b08c65307
김현영 <hggew@naver.com>
2024-02-14 21:23:54 +0900

    feat: 그림활동 책 css
    Former-commit-id: 83c85546fc5b18f38608ce0c88f0d5abde908c18

--- frontend/src/pages/art/Coloring.jsx
+++ frontend/src/pages/art/Coloring.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7587a651702b1a41856656fbee5bf00d886f97657c335263285b990124fad943
-size 15075
+oid sha256:5506d4244db1c03216bcd34ddebb203da1955109dff41dbe46f09d03f0ba12dd
+size 15389
--- frontend/src/pages/main/MainPage.jsx
+++ frontend/src/pages/main/MainPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4ba62ca2441dde73cc0b0480b171fd347a81a4619b8ff9754f1664920ff54f55
-size 2386
+oid sha256:4657c4a5002eb90fc9b4b3d7a4071ed8a8c5b0bb0ddd7c46a389a9f63438f207
+size 2412
