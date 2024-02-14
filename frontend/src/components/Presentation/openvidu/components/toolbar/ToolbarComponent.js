dce403a4b992266622c4dee4a89a9b827b06894e
오기선 <pakox123@gmail.com>
2024-02-14 13:26:56 +0900

    feat : 툴바제목제거
    Former-commit-id: aa904c2fd54c8473e4bde77d23aeb4ecb2c559fe

--- frontend/src/components/Presentation/openvidu/components/toolbar/ToolbarComponent.js
+++ frontend/src/components/Presentation/openvidu/components/toolbar/ToolbarComponent.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fdbed859bcff759c6f70d9274b83c927df77cfade49ea5292b33ad4af8c816f8
-size 4630
+oid sha256:2985a42bdcd07a2db1eb45357e356544227ceae08a7c4348398555d8658579f6
+size 4401
