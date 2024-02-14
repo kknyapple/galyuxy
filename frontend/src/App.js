e70800cb2ad8d2936787c538453ed47800c68c37
kkongch <dhwjdals1324@gmail.com>
2024-02-15 02:55:18 +0900

    feat : quiz api 수정
    Former-commit-id: f784ca2d3ecac55e516dc09e251cc83c6d3a5068

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:12b4ee398e423fa2553c9b7c32f4d385bdc8c44f07158f22bda7a27da6311219
-size 6096
+oid sha256:0ff474b51b4f68fb7b1669e577188f926a85731d43109b973531e10b8e8fe051
+size 6100
--- frontend/src/components/Quiz/QuizModal.jsx
+++ frontend/src/components/Quiz/QuizModal.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1a742cd843a763e46bfaa940d22c5383f05e9e5750a7e36cabb72814b7958568
-size 7259
+oid sha256:ccc6ff28e7d84db45f26fe0caf9d1a459663a5833281ac32c22ea5d9fdc1c5e3
+size 7365
