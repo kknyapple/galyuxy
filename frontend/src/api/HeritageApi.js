fb11db84e13b1ca81d7262afb1a9dcbb6c3c8883
nayeon <kknyapple@gmail.com>
2024-02-14 16:14:03 +0900

    feat: ear api 추가
    GET /ear
    
    
    Former-commit-id: e74290e802c9a05a17ce499e2bd38e53ae72ca74

--- frontend/src/api/HeritageApi.js
+++ frontend/src/api/HeritageApi.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aeceb3d1ea15376f0445bb383e46d7a61f581f99d4ff1cbdc3bf73d05ea4c3e3
-size 587
+oid sha256:9b07b6d4330eed511b6c17726f31d9bb69f762fd2361e5a722f50831d08171fe
+size 807
