f1140025c446d18873e39ade84f00c2059790cdb
오정민 <dhwjdals1324@gmail.com>
2024-02-14 14:56:58 +0900

    feat : quizdetailteacher api 붙이기 완료
    Former-commit-id: fc51551b48ad35e81800b0cf1765f8deaa59e15c

--- frontend/src/api/HeritageApi.jsx
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:ff2b39376d011ae51fed2b1d625430f2036ef8c23ea447aefd7c4eaf82856e0a
-size 979
--- frontend/src/pages/quiz/teacher/QuizDetailTeacher.jsx
+++ frontend/src/pages/quiz/teacher/QuizDetailTeacher.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f69c1ce70469e058aed074110b994c88f2b2ef0efe2a25ded0f6589543a784df
-size 5966
+oid sha256:7a68edf161acab451ca85af2ed46aaccb4128c47278b050e96d609d78223f737
+size 6312
