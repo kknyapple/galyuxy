e9e2317a4261c5eafa3a90c69f478fb4aa4d7e8f
nayeon <kknyapple@gmail.com>
2024-02-15 00:11:32 +0900

    feat: 미술활동 펜, 지우개 select에서 버튼으로 변경
    Former-commit-id: 057da4808ae78253c9f21c37fb5c910c2f780105

--- frontend/src/pages/art/Coloring.jsx
+++ frontend/src/pages/art/Coloring.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5506d4244db1c03216bcd34ddebb203da1955109dff41dbe46f09d03f0ba12dd
-size 15389
+oid sha256:19061a0aaedde8f1433e568ab9ac646a6eab595c70563e0fdf2f24be0e4dc15d
+size 16881
