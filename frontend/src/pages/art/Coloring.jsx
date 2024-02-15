4232404c58a0615f17077ee15e52cb923036463a
nayeon <kknyapple@gmail.com>
2024-02-15 11:49:04 +0900

    design: 컬러링북 canvas 가운데 정렬
    Former-commit-id: 7abe39917b7557c6f953368b9b018f6dff047ac0

--- frontend/src/pages/art/Coloring.jsx
+++ frontend/src/pages/art/Coloring.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:734a17945d274c29c4ce09ae9ba08f5865c7e0fb9b97983c6eae931b2b5ded8f
-size 19863
+oid sha256:b34958e648f15260b4f96d60cedad479855b488c61df65ea3a432e3148183485
+size 20530
