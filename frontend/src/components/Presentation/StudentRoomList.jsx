07c013fddc9a5184dc094d6bb896537411de2b2b
nayeon <kknyapple@gmail.com>
2024-02-15 09:42:36 +0900

    design: css 수정
    Former-commit-id: 455c4a5536b4c9772cdb7104c1415f19e2ae4ea9

--- frontend/src/components/Presentation/StudentRoomList.jsx
+++ frontend/src/components/Presentation/StudentRoomList.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f5ed8c7e14ec5b7fd75b61fba6ef905627229a31dc404b1b04f99a986492b14c
+oid sha256:fd2968e428795f89be9d0d6ed526757aa852b438041f99a7e6ac0b5234314dc9
 size 3320
