9d044ddef6e9fc35540cd8a79b47182fba95d7ee
김현영 <hggew@naver.com>
2024-02-15 12:12:43 +0900

    feat: 카메라 위 3d 모델 ar 올리기
    Former-commit-id: 348a85022e148d4688f32899cf435d238a459cbb

--- frontend/src/pages/art/TestTest.jsx
+++ frontend/src/pages/art/TestTest.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0cbfe226843c0b8f1938181021bdd76020522432e28cc131b68e85e768d6b436
-size 9562
+oid sha256:dd12c16c4b59164ffcd40a7a246cd90a67aae61939f68dcdf7b382bd4ecea311
+size 9213
