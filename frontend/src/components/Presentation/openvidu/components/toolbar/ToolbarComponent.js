19a971b51a19103b0e69aaca1f64e304be6e2302
오기선 <pakox123@gmail.com>
2024-02-16 00:32:19 +0900

    feat : 툴바페이지 주석제거
    Former-commit-id: fefec132de9c5868973faf47d4370b610b148207

--- frontend/src/components/Presentation/openvidu/components/toolbar/ToolbarComponent.js
+++ frontend/src/components/Presentation/openvidu/components/toolbar/ToolbarComponent.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fe186198450c3d47188d5ebc66656644d840bee1e9c8aaab2938cd90920d2c59
-size 3609
+oid sha256:bdf1a04e0b2232436ef6239b0a3403e5148f3dd029df131950a0820615f98ac9
+size 3285
